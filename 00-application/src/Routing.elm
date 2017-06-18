4627c8e378ec6f6687e222e25d110eb199260de9
sdia <seydou.dia@olx.com>
2017-06-18 23:15:46 +0200

    navigation is done

--- 00-application/src/Routing.elm
+++ 00-application/src/Routing.elm
@@ -23,3 +23,13 @@ parseLocation location =
 
         Nothing ->
             NotFoundRoute
+
+
+playersPath : String
+playersPath =
+    "#players"
+
+
+playerPath : PlayerId -> String
+playerPath id =
+    "#players/" ++ id
